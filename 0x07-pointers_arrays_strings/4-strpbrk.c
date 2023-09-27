cc6b5e0d42299d79ab09adaa9850b40b29802a0a
Meriemeghz <meriemeghzaouni@gmail.com>
2023-09-26 17:04:28 -0700

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "mainh"
+
+/**
+ * _strpbrk- intry point
+ * @s: input
+ * @accept: input
+ * Return: always (success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = ; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+	s++;
+	}
+	return ('\0');
+}
